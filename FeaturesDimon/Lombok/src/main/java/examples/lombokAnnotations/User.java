bb0122e448fe2c52e3bad0908565b05c95e024a7
dmitry.chernetsov <dmitry.chernetsov@ntlab.com>
2021-06-14 20:28:58 +0300

    delete FeaturesDimon from master

--- FeaturesDimon/Lombok/src/main/java/examples/lombokAnnotations/User.java
+++
@@ -1,8 +0,0 @@
-package examples.lombokAnnotations;
-
-public class User {
-    public static void main(String[] args) {
-        Person person = new Person();
-        person.setName("dawawd");
-    }
-}
--- FeaturesDimon/SimpleCrudProject/src/main/java/my/app/servlet/PersonUpdateServlet.java
+++
@@ -1,8 +0,0 @@
-package my.app.servlet;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet
-public class PersonUpdateServlet extends HttpServlet {
-}
