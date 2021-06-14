b70c8438bc2219b8c14cf2924e7afe099759eb37
dmitry.chernetsov <dmitry.chernetsov@ntlab.com>
2021-06-14 21:54:09 +0300

    add FeaturesDimon module

---
+++ FeaturesDimon/SimpleCrudProject/src/main/java/my/app/servlet/PersonCreateServlet.java
@@ -0,0 +1,50 @@
+package my.app.servlet;
+
+import com.google.gson.Gson;
+import my.app.entity.Person;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/api/person")
+public class PersonCreateServlet extends HttpServlet {
+
+    {
+        System.out.println();
+    }
+
+
+    public PersonCreateServlet() {
+        System.out.println();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String idString = req.getParameter("id");
+        String name = req.getParameter("name");
+        String ageString = req.getParameter("age");
+
+        Person person = new Person(Long.parseLong(idString),name,Integer.parseInt(ageString));
+        PersonListServlet.personList.add(person);
+
+        Gson gson = new Gson();
+        String personJsonString = gson.toJson(person);
+
+        PrintWriter out = resp.getWriter();
+        out.print(personJsonString);
+        out.flush();
+    }
+
+
+}
