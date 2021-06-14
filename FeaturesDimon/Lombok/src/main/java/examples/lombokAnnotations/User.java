581e5b88d72238ba52c7a672df9911157e77478f
dmitry.chernetsov <dmitry.chernetsov@ntlab.com>
2021-06-14 19:20:15 +0300

    initial commit

---
+++ FeaturesDimon/Lombok/src/main/java/examples/lombokAnnotations/User.java
@@ -0,0 +1,8 @@
+package examples.lombokAnnotations;
+
+public class User {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("dawawd");
+    }
+}
---
+++ FeaturesDimon/SimpleCrudProject/src/main/java/my/app/servlet/PersonUpdateServlet.java
@@ -0,0 +1,8 @@
+package my.app.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet
+public class PersonUpdateServlet extends HttpServlet {
+}
