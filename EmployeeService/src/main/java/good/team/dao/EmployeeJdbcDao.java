241c1c1476164e7216123c1d7cecd613db7d5009
dmitry.chernetsov <dmitry.chernetsov@ntlab.com>
2021-06-21 21:59:05 +0300

    SDE-21 added EmployeeJdbcDao

--- EmployeeService/src/main/java/good/team/dao/EmployeeJdbcDao.java
+++ EmployeeService/src/main/java/good/team/dao/EmployeeJdbcDao.java
@@ -1,4 +1,20 @@
 package good.team.dao;
 
-public class EmployeeJdbcDao {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class EmployeeJdbcDao implements EmployeeDao {
+
+    @Override
+    public void deleteEmployeeById(int id) {
+        try (Connection connection = new JdbcUtil().getConnection();
+             Statement statement = connection.createStatement()) {
+
+            String sql = "DELETE FROM `ppVLywhE9a`.`employee` WHERE (`id` = '" + id + "');";
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
