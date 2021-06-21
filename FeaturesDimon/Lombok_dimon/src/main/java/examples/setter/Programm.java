be1f79e82a001d04d674cc91c26c6033f03701ed
kokanden <kokantut@gmail.com>
2021-06-21 19:54:58 +0300

    deleting modules featuresDimon&Serg

--- FeaturesDimon/Lombok_dimon/src/main/java/examples/setter/Programm.java
+++
@@ -1,8 +0,0 @@
-package examples.setter;
-
-public class Programm {
-    public static void main(String[] args) {
-        Person person = new Person();
-        person.setName("serg");
-    }
-}
--- FeaturesDimon/SimpleCrudProject_dimon/src/main/java/my/app/servlet/PersonUpdateServlet.java
+++
@@ -1,8 +0,0 @@
-package my.app.servlet;
-
-import javax.servlet.http.HttpServlet;
-
-public class PersonUpdateServlet extends HttpServlet {
-
-
-}
