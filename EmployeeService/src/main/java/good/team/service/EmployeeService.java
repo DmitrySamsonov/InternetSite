d5d2a3ff997f600998837334b28bb3a7c9d10117
Skomorokh 1985 <Torredo1985@outlook.com>
2021-06-27 22:45:26 +0300

    SDE-20 creating  method getAllEmployees in EmployeeService

--- EmployeeService/src/main/java/good/team/service/EmployeeService.java
+++ EmployeeService/src/main/java/good/team/service/EmployeeService.java
@@ -1,13 +1,14 @@
 package good.team.service;
 
 import good.team.dao.EmployeeDao;
+import good.team.dao.EmployeeJdbcDao;
 import good.team.entity.Employee;
 
 import java.util.List;
 
 public class EmployeeService {
 
-    EmployeeDao employeeDao;
+    EmployeeDao employeeDao = new EmployeeJdbcDao();
 
     public List<Employee> getAllEmployee() {
         List<Employee> employees = employeeDao.getAllEmployee();
