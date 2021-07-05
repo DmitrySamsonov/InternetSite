933872812409593ae5f05f99154e85676a030c9c
Alexandr-AP <petrusha_as@mail.ru>
2021-07-05 23:01:41 +0300

    add webUI module.

---
+++ webUI/src/test/java/good/team/webUI/WebUiApplicationTests.java
@@ -0,0 +1,13 @@
+package good.team.webUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
