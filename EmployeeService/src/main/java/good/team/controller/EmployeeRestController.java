8a3e5a40729a66f84ec0588ce8079947686780fd
Skomorokh 1985 <Torredo1985@outlook.com>
2021-07-05 19:48:33 +0300

    update EmployeeController to EmployeeRestController

--- EmployeeService/src/main/java/good/team/controller/EmployeeRestController.java
+++ EmployeeService/src/main/java/good/team/controller/EmployeeRestController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
-public class EmployeeController {
+public class EmployeeRestController {
 
     @Autowired
     EmployeeService employeeService;
